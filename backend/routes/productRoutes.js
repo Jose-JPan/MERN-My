171af1d71fe59d0b735a80809ce183a8ab3171ed
Jose-Jpan <jose.santos.soares.nz@gmail.com>
2025-04-14 00:20:22 +0900

    Many updates

---
+++ backend/routes/productRoutes.js
@@ -0,0 +1,11 @@
+import express from 'express';
+const router = express.Router();
+import productController from '../controllers/productController.js';
+
+router.get('/', productController.getAllProducts);
+router.get('/:id', productController.getProduct);
+router.post('/', productController.createProduct);
+router.put('/:id', productController.updateProduct);
+router.delete('/:id', productController.deleteProduct);
+
+export default router;
--- backend/server.js
+++ backend/server.js
@@ -1,48 +1,23 @@
-const express = require('express');
-const cors = require('cors');
-// const productRoutes = require('./routes/productRoutes');
+import express from 'express';
+import cors from 'cors';
 const PORT = 5050;
 const app = express();
-const mysql = require('mysql2'); // or 'mysql'
-
-// app.use(cors());
+import connectWithRetry from './config/db.js';
+app.use(cors());
 app.use(express.json()); // to parse JSON bodies
 
-// app.use('/products', productRoutes);
+//app.use('/products', productRoutes);
+
+// Requiring routes
+import productRoutes from './routes/productRoutes.js';
 
 // const PORT = process.env.PORT || 5000;
 // app.listen(PORT, () => console.log('Server running on port ${PORT}'));
 
-const dbConfig= {
-    host: 'mysql', 
-    port: 3306,
-    user: 'user',
-    password: 'userpassword', 
-    database: 'mydatabase'
-};
-
-function connectWithRetry() {
-    const connection = mysql.createConnection(dbConfig);
-    connection.connect((err) => {
-        if (err) {
-            console.error('Failed to connect to DB. Retrying in 5 seconds...', err.message);
-            setTimeout(connectWithRetry, 5000); // Retry after 5 seconds
-        } else {
-            console.log('Connected to the database.');
-
-            app.get('/products', (req, res) => { connection.query('SELECT * FROM tblProdutos', (err, results) => {
-                    if (err) return res.status(500).send(err);
-                    res.json(results);
-                });
-            });
-
-            app.listen(PORT, '0.0.0.0', () => {
-                console.log('Server running on port' + ' ' + (PORT));
-            });
-        }
-// test        
-
-    });
-}
-
 connectWithRetry();
+
+app.listen(PORT, '0.0.0.0', () => {
+    console.log('Server running on port' + ' ' + (PORT));
+});
+ 
+
