23a28bb58de62b8d3bcb9f11db48b43c61551aa8
Jose-Jpan <jose.santos.soares.nz@gmail.com>
2025-04-17 17:02:05 +0900

    Now it is running the Server

--- backend/models/productModel.js
+++ backend/models/productModel.js
@@ -1,19 +1,19 @@
-import db from '../config/db.js';
+import pool, { connectDB } from '../config/db.js';
 
 const Product = {
-
+    
     getAll: async () => {
         try {
-            const [rows] = await db.query('SELECT * FROM tblProdutos');
+            const [rows] = await pool.query('SELECT * FROM tblProdutos');
             return rows;
         } catch (err) {
           throw err;
         }
      },
-
+    
     getById: async (id) => {
         try {
-            const [rows] = await db.query('SELECT * FROM tblProdutos WHERE id = ?', [id]);
+            const [rows] = await pool.query('SELECT * FROM tblProdutos WHERE ProdutoID= ?', [id]);
             return rows[0]; // Return the first product (or undefined if not found)
         } catch (err) {
           throw err;
--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -3,9 +3,9 @@ const router = express.Router();
 import productController from '../controllers/productController.js';
 
 router.get('/', productController.getAllProducts);
-router.get('/:id', productController.getProduct);
-router.post('/', productController.createProduct);
-router.put('/:id', productController.updateProduct);
-router.delete('/:id', productController.deleteProduct);
+router.get('/:id', productController.getProductById);
+//router.post('/', productController.createProduct);
+//router.put('/:id', productController.updateProduct);
+//router.delete('/:id', productController.deleteProduct);
 
 export default router;
