7be7e4def710314e8fe117f75928e0f6df11cae7
Jose-Jpan <jose.santos.soares.nz@gmail.com>
2025-05-14 12:03:49 +0900

    A few updates

--- frontend/src/api/productApi.js
+++ frontend/src/api/productApi.js
@@ -1,19 +1,18 @@
-import axios from 'axios';
+import api from './axiosInstance';
 
-// Base URL for the backend
-const API_URL = 'http://localhost:5050/products';
+console.log("API BASE URL:", process.env.REACT_APP_API_URL);
 
 // Get all products
-export const getAllProducts = () => axios.get(API_URL);
+export const getAllProducts = () => api.get('/products');
 
 // Get a single product by ID
-export const getProductById = (id) => axios.get(`${API_URL}/${id}`);
+export const getProductById = (id) => api.get(`/products/${id}`);
 
 // Create a new product
-export const createProduct = (data) => axios.post(API_URL, data);
+export const createProduct = (data) => api.post('/products', data);
 
 // Update a product
-export const updateProduct = (id, data) => axios.put(`${API_URL}/${id}`, data);
+export const updateProduct = (id, data) => api.put(`products/${id}`, data);
 
 // Delete a product
-export const deleteProduct = (id) => axios.delete(`${API_URL}/${id}`);
+export const deleteProduct = (id) => api.delete(`products/${id}`);
